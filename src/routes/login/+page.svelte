4abd9d488a4e2ff3d184a25628eff68f4c3c53dd
fcaramez <francisco.caramez.duarte@gmail.com>
2023-07-26 00:12:45 +0100

    adds login

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -1,11 +1,60 @@
+<script lang="ts">
+	import { goto } from '$app/navigation';
+	import { useAxios } from '$lib';
+	import { toasts, ToastContainer, FlatToast } from 'svelte-toasts';
+
+	let handle: string;
+	let password: string;
+	const handleSubmit = async () => {
+		try {
+			const apiResponse = await useAxios.post(`/auth/login`, {
+				username: handle,
+				email: handle,
+				password
+			});
+
+			const { data, message } = apiResponse.data;
+
+			const { authToken } = data;
+
+			localStorage.setItem('authToken', authToken);
+			toasts.add({
+				title: 'Signed up!',
+				description: message,
+				duration: 3000,
+				placement: 'top-center',
+				theme: 'dark',
+				showProgress: true,
+				type: 'success'
+			});
+			await goto('/');
+		} catch (error: any) {
+			toasts.add({
+				title: 'Error signing up',
+				description: error.body.message,
+				duration: 3000,
+				placement: 'top-center',
+				theme: 'dark',
+				showProgress: true,
+				type: 'error'
+			});
+			alert(error);
+		} finally {
+			handle = '';
+			password = '';
+		}
+	};
+</script>
+
 <main class="container">
 	<div>
 		<hgroup>
 			<h1>Sign in</h1>
 			<h2>To enjoy all our cool features</h2>
 		</hgroup>
-		<form>
+		<form on:submit|preventDefault={handleSubmit} method="POST">
 			<input
+				bind:value={handle}
 				type="text"
 				name="Username"
 				placeholder="Username or Email"
@@ -14,6 +63,7 @@
 				required
 			/>
 			<input
+				bind:value={password}
 				type="password"
 				name="password"
 				placeholder="Password"
